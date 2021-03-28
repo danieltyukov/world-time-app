4cedbad551427b42e363c8005383c820b59b6272
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-03-28 22:35:05 +0300

    some routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Home(),
-      ),
+      MaterialApp(home: Home(), routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      }),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+
 class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(),
+    return Scaffold(
+      body: SafeArea(
+        child: SafeArea(
+          child: Text("Home"),
+        ),
+      ),
     );
   }
 }
