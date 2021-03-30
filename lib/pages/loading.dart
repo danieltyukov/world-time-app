532c3d4e90dfdb181d22b8357d0b1718c8b9bee0
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-03-30 17:43:35 +0300

    some updates

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   Loading({Key key}) : super(key: key);
@@ -9,22 +10,37 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Response response = await get(
-      Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
-    );
+  void getTime() async {
+    try {
+      Response response = await get(
+        Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'),
+      );
+      Map data = jsonDecode(response.body);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(
+        Duration(
+          hours: int.parse(offset),
+        ),
+      );
+    } on Exception catch (error) {
+      print(error);
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Loading Sreen')),
+      body: SafeArea(
+        child: Text('Loading Sreen'),
+      ),
     );
   }
 }
